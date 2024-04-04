3fd8b8297936aba19fd37506055e254ac428679a
Michael Wilson <shortguy014@gmail.com>
2024-04-04 15:41:40 +1000

    fix: update favicon and head

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-theme-cody.netlify.app',
+  site: 'https://mwilson.io',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
